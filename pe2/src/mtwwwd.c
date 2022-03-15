4249542d1924950fb0947b8f60479252de382d10
Elias Borge Svinø <eliasbosv@gmail.com>
2022-03-15 19:45:30 +0100

    Cleanup

--- pe2/src/mtwwwd.c
+++ pe2/src/mtwwwd.c
@@ -37,30 +37,7 @@ void server_close_all(server *, const int) {};
 int server_open(server *, long, short) {};
 
 
-//Connection functions
 
-void conn_clear(conn_t *){};
-
-
-void conn_close(conn_t *){};
-
-int conn_open(conn_t *, long, short){};
-
-int conn_accept(server *, conn_t *){};
-
-void conn_clear_all(conn_t *, const int){};
-
-void conn_close_all(conn_t *, const int){};
-
-int conn_add(conn_t *, const int, conn_t *){};
-
-int conn_close_del(conn_t *, const int, conn_t *){};
-
-int conn_wait_to_read(conn_t *, struct timeval *){};
-
-int conn_write(conn_t *, const void *, int){};
-
-int conn_read(conn_t *, void *, int){};
 
 int main(int argc, char *argv[]){
     
