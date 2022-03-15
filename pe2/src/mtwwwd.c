4ad3505eb04e4f221d02c87cc157ed57e6b597d5
TommyTom Frettis <thomas.frette@gmail.com>
2022-03-15 19:58:55 +0100

    startup

--- pe2/src/mtwwwd.c
+++ pe2/src/mtwwwd.c
@@ -1,46 +1,24 @@
-#include <winsock2.h>
+#define MAXREQ (4096 * 1024)
+#define PORT 6789
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netdb.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
 
 
+char buffer[MAXREQ] , body[MAXREQ] , msg[MAXREQ] ;
+void error(const char *msg) { perror(msg) ; exit(1); }
 
-typedef struct server {
-    int socket;
-    struct sockaddr_in address;
-}
-server;
-typedef struct conn_t {
-    int socket;
-    struct sockaddr_in address;
-    struct sockaddr_in local;
-}
-conn_t;
+int main(void) {
 
-//Server functions
-void server_clear(server * server){
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    struct  sockaddr_in serv_addr, cli_addr;
     
-};
-
-void server_clear_all (server * server, int num) {
-
-};
-void server_close(server *){};
-
-void server_close_all(server *, const int) {};
-
-int server_open(server *, long, short) {};
-
-
-
-
-int main(int argc, char *argv[]){
     
-
-    return 0;
-};
+}
