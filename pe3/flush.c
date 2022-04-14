5d50d2d77be87a355441f313b53fcb806ec51f7d
eliasborge <94860474+eliasborge@users.noreply.github.com>
2022-04-14 14:47:18 +0700

    Elias (#8)
    * add else statements
    
    * Start of task 3. Implement cwd functionality.
    
    * implement shutdown fucntionality, split command string

--- pe3/flush.c
+++ pe3/flush.c
@@ -8,32 +8,91 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define MAXIMUM_PATH_LENGTH 4096;
+
+#define MAXIMUM_PATH_LENGTH 4096
+#define MAXIMUM_COMMAND_AMOUNT 64
 
 char cwd[MAXIMUM_PATH_LENGTH];
+char r_d[MAXIMUM_PATH_LENGTH];
 
-//variables
+// variables
 const char SPACE = 0x20;
 const char TAB = 0x09;
-const char CTRLD= 0x04;
+// This is not actually CTRL-D, but ctrl d shuts down in the wrong way. A simple d will turn off the application.
+const char CTRLD = 0x64;
 
-
-void take_arguments(){
-    //Implement method for reading the input string. Splitting on SPACE or TAB and then call method for
-    // creating the fork process and exectuing the args. 
+void take_arguments()
+{
+    // Implement method for reading the input string. Splitting on SPACE or TAB and then call method for
+    //  creating the fork process and exectuing the args.
 }
 
+void print_current_working_directory()
+{
 
-void print_current_working_directory(){
-    cwd = getcwd();
-
-    if(cwd == NULL){
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
         perror("Error while getting current working directory");
     }
-    else{
-        printf("%s:",cwd);
+    else
+    {
+        printf("\n");
+        printf("%s:", cwd);
+    }
+}
+
+int main()
+{
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
+        perror("Error while getting current working directory");
     }
 
-    take_arguments();
-    
+    memcpy(r_d, cwd, sizeof(cwd));
+    while (1)
+    {
+        print_current_working_directory();
+        char command;
+        scanf(" %s", &command);
+
+        int ascii = command;
+        if (ascii == CTRLD)
+        {
+            printf("Shutting Down\n");
+            break;
+        }
+
+        else
+        {
+            // Split string on SPACE and TAB and send to different method.
+            // src: https://riptutorial.com/c/example/2557/tokenisation--strtok----strtok-r---and-strtok-s--
+            // Had to concat Tab onto space because i didnt know any other way.
+            char delimiters[2] = {SPACE, TAB};
+            char splittedCommands[MAXIMUM_COMMAND_AMOUNT];
+            char *token;
+            int toknum = 0;
+            token = strtok(&command, delimiters);
+            while (token != NULL)
+            {
+                //Inserting commands into an array
+                if(toknum > MAXIMUM_COMMAND_AMOUNT -1){
+                    perror("Command too long");
+                }
+
+                splittedCommands[toknum] = token;
+
+                token = strtok(NULL, delimiters);
+
+                toknum++;
+            }
+
+            printf("Works");
+            //Unable to print array of tokens but i think it is good to go. 
+            //Create function that sends commands to executing function. 
+        }
+    }
+
+    return 1;
+
 }
