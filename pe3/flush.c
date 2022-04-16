ded4cfc2c2f032c5e62b856f5a0376e1ec8c08d8
TommyTom Frettis <thomas.frette@gmail.com>
2022-04-16 16:02:49 +0200

    Commiter før jeg endrer til fgets

--- pe3/flush.c
+++ pe3/flush.c
@@ -22,6 +22,7 @@ const char TAB = 0x09;
 // This is not actually CTRL-D, but ctrl d shuts down in the wrong way. A simple d will turn off the application.
 const char CTRLD = 0x64;
 
+//Lagret disse globalt sånn at man lettere kunne nå de gjennom forskjellige funksjone
 char *splittedCommands[MAX_BUFFER_LENGTH];
 char *mainCommand;
 
@@ -29,6 +30,21 @@ void take_arguments()
 {
     // Implement method for reading the input string. Splitting on SPACE or TAB and then call method for
     //  creating the fork process and exectuing the args.
+
+    //Check what the main command is, https://www.geeksforgeeks.org/chdir-in-c-language-with-examples/
+    if(strcmp(mainCommand, "cd") == 0){
+
+        //Sjekker at den det første som ligger i splittedCommands eksisterer
+        if(splittedCommands[0] != NULL) {
+            
+            chdir(splittedCommands[0]);
+        } else {
+            printf("%s", splittedCommands[0]);
+        }
+    }
+
+    //https://www.delftstack.com/howto/c/execvp-in-c/#:~:text=Use%20execvp%20Function%20to%20Replace%20a%20Process%20Image%20in%20C,-In%20Unix%2Dbased&text=These%20functions%20take%20a%20file,arguments%20as%20the%20second%20argument
+
 }
 
 void print_current_working_directory()
@@ -84,16 +100,14 @@ int main()
                 if(toknum > MAXIMUM_COMMAND_AMOUNT -1){
                     perror("Command too long");
                 }
-
                 token = strtok(NULL, delimiters);
                 splittedCommands[toknum] = token;
                 toknum++;
             }
-            printf("Works");
 
             //Sender deretter denne til take_arguments som utfører kommandoene
             
-
+            take_arguments();
 
 
 
