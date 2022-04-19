20ae17e9c26faa285ce27457e4ec0e22128f9366
Elias Borge Svinø <eliasbosv@gmail.com>
2022-04-19 14:51:50 +0200

    Fix jobs and cathing zombies. Ampersand removal remains and I/O handling.

--- pe3/flush.c
+++ pe3/flush.c
@@ -18,26 +18,27 @@ char cwd[MAXIMUM_PATH_LENGTH];
 char r_d[MAXIMUM_PATH_LENGTH];
 
 bool backgroundTask = false;
+int ampersandIndex;
 
 struct ChildProcess
 {
     pid_t pid;
-    char main;
+    char main[20];
     bool isActive;
     char extraCommands[MAX_BUFFER_LENGTH];
 };
 
 struct ChildProcess background_processes[MAX_BACKGROUND_TASKS];
 
-// variables // vet ikke om vi trenger disse
+// variables
 const char SPACE = 0x20;
 const char TAB = 0x09;
 // This is not actually CTRL-D, but ctrl d shuts down in the wrong way. A simple d will turn off the application.
-const char CTRLD = 0x64;
+const char CTRLD = 0x04;
 
 // Lagret disse globalt sånn at man lettere kunne nå de gjennom forskjellige funksjone
 char *splittedCommands[MAX_BUFFER_LENGTH];
-char *mainCommand;
+char *mainCommand[20];
 
 void print_current_working_directory()
 {
@@ -70,24 +71,26 @@ void change_dir()
 
 void jobs()
 {
-    
-    
 
     for (int i = 0; i < MAX_BACKGROUND_TASKS; i++)
     {
         struct ChildProcess cp = background_processes[i];
-        for (int i = 0; i < sizeof(cp.extraCommands); i++)
-        {
-            if(i == MAX_BACKGROUND_TASKS -1 && !cp.isActive){
-                printf("\nThere are no active background tasks");
-                return;
-            }
 
-            if(!cp.isActive){
-                continue;
-            }
-            printf("\n %d: %s %s", cp.pid, cp.main, cp.extraCommands[i]);
+        // If I is at the final possible process and it is not active.
+        if (i == 0 && !cp.isActive)
+        {
+            printf("\nThere are no active background tasks");
+            return;
         }
+        // If process is inactive, continue to next.
+        if (!cp.isActive)
+        {
+            
+            continue;
+        }
+            
+            printf("\n %d: %s", cp.pid, cp.main);
+        
     }
 }
 
@@ -108,55 +111,54 @@ void execute_cd_or_jobs()
 void execute_wait_operation()
 {
 
-    
-
     // Source: https://www.youtube.com/watch?v=DiNmwwQWl0g
     // Source: https://www.delftstack.com/howto/c/execvp-in-c/#:~:text=Use%20execvp%20Function%20to%20Replace%20a%20Process%20Image%20in%20C,-In%20Unix%2Dbased&text=These%20functions%20take%20a%20file,arguments%20as%20the%20second%20argument
 
     pid_t child_pid = fork();
-    
+
     int wait_status;
+
+    // Lagrer informasjon om prosessen.
+    struct ChildProcess currentProcess;
+    currentProcess.pid = child_pid;
+    strcpy(currentProcess.main,mainCommand);
+    currentProcess.isActive = true;
+    // måtte lage loop. Klarte ikke sette hele arrayet på en linje.
+    for (int i = 0; i < sizeof(currentProcess.extraCommands); i++)
+    {
+        currentProcess.extraCommands[i] = splittedCommands[i];
+    }
+
+    // plasserer structen i arrayet med bakgrunnsprosesser.
+    for (int i = 0; i < MAX_BACKGROUND_TASKS; i++)
+    {
+        if (background_processes[i].isActive == false)
+        {
+            background_processes[i] = currentProcess;
+            break;
+        }
+
+        if (i == MAX_BACKGROUND_TASKS - 1 && background_processes[MAX_BACKGROUND_TASKS - 1].isActive)
+        {
+            printf("Background task array full");
+        }
+    }
 
     if (child_pid == -1)
     {
         printf("Wrong!!!");
         exit(EXIT_FAILURE);
-
-        // In the childprocess
     }
+
+    // In the childprocess
     else if (child_pid == 0)
     {
 
-        // Lager informasjon om prosessen.
-        struct ChildProcess currentProcess;
-        currentProcess.pid = child_pid;
-        currentProcess.main = *mainCommand;
-        currentProcess.isActive = true;
-        // måtte lage loop. Klarte ikke sette hele arrayet på en linje.
-        for (int i = 0; i < sizeof(currentProcess.extraCommands); i++)
-        {
-            currentProcess.extraCommands[i] = splittedCommands[i];
-        }
-
-        // plasserer structen i arrayet med bakgrunnsprosesser.
-        for (int i = 0; i < MAX_BACKGROUND_TASKS; i++)
-        {
-            if (background_processes[i].isActive == false)
-            {
-                background_processes[i] = currentProcess;
-            }
-
-            if (i == MAX_BACKGROUND_TASKS - 1 && background_processes[MAX_BACKGROUND_TASKS - 1].isActive)
-            {
-                printf("Background task array full");
-            }
-        }
-        
         int exec;
 
-        
-        exec = execvp(mainCommand,splittedCommands);
-        if(exec == -1){
+        exec = execvp(mainCommand, splittedCommands);
+        if (exec == -1)
+        {
             printf("error with execvp");
         }
         exit(0);
@@ -165,7 +167,7 @@ void execute_wait_operation()
     {
         // Parent process from YT video
         // wait(wait_status);
-        
+
         if (waitpid(child_pid, &wait_status, WUNTRACED) == -1)
         {
             printf("Perror: waitpid.\n");
@@ -176,6 +178,14 @@ void execute_wait_operation()
         {
             int statusCode = WEXITSTATUS(wait_status);
             printf("\nExit status [%s %s] = %d", mainCommand, splittedCommands[1], statusCode);
+            //Catch the zombies. After process is finished, remove its boolean value and it wont appear for user. 
+            for (int i = 0; i < 32; i++)
+            {
+                if(background_processes[i].pid == child_pid){
+                    background_processes[i].isActive = false;
+                }
+            }
+            
         }
     }
 }
@@ -187,7 +197,6 @@ int main()
     {
         background_processes[i].isActive = false;
     }
-    
 
     if (getcwd(cwd, sizeof(cwd)) == NULL)
     {
@@ -225,7 +234,7 @@ int main()
             char *token = strtok(&command, delimiters);
 
             // Lagrer hovedkommandoen i en egen variabel, samler resten i splittedCommands
-            mainCommand = token;
+            strcpy(mainCommand,token);
             // Måtte legge til den første og ellers funker ikke execvp
             splittedCommands[toknum] = mainCommand;
 
@@ -237,13 +246,26 @@ int main()
                     perror("Command too long");
                 }
                 token = strtok(NULL, delimiters);
-                if(strcmp(token,"&") == 0){
-                    backgroundTask = true;
-                    token = "";
-                    break;
-                }
+
                 splittedCommands[toknum + 1] = token;
                 toknum++;
+            }
+            // Må sjekke om det skal være en background process.
+            if (splittedCommands[strlen(splittedCommands) - 1] == '&')
+            {
+
+                backgroundTask = true;
+
+                for (int i = 0; i < MAX_BUFFER_LENGTH; i++)
+                {
+                    char *ret = strstr(splittedCommands[i], "&");
+                    if (ret)
+                    {
+                        splittedCommands[i] = "";
+                    }
+                }
+
+                printf("BG");
             }
 
             // Hvis det ikke er noen kommandoer i det hele tatt
@@ -270,7 +292,7 @@ int main()
 
             // reset
             memset(splittedCommands, 0, MAX_BUFFER_LENGTH * sizeof(splittedCommands[0]));
-            mainCommand = NULL;
+            //mainCommand = "\0";
         }
     }
 
